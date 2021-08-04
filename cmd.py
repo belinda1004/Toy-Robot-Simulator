e5c0006c862a7b5d3a277203452a373142a62c07
belinda1004 <belinda.yuting.wang@live.com>
2021-08-04 12:14:35 +1000

    sturcture of class ToyRobot
    1, Finish the sturcture of class ToyRob ot
    2, Finish the basic logic of proc_cmd()

--- cmd.py
+++ cmd.py
@@ -3,4 +3,29 @@ Check and execute commands
 """
 
 def proc_cmd():
-    pass
+    command_str = input("Command: ").upper()
+    command_list = command_str.split(" ")
+    command = command_str[0]
+
+    if not command in ["PLACE", "MOVE", "LEFT", "RIGHT", "REPORT"]:
+        print("Unknown Command.")
+        return
+
+    if (command != "PLACE" and len(command_list) > 1) or \
+        (command == "PLACE" and len(command_list) != 4):
+        print("Invalid arguments.")
+        return
+
+    if command == "PLACE":
+        pass
+    elif command == "MOVE":
+        pass
+    elif command == "LEFT":
+        pass
+    elif command == "RIGHT":
+        pass
+    elif command == "REPORT":
+        pass
+    return
+
+
--- toy_robot.py
+++ toy_robot.py
@@ -3,4 +3,26 @@ The definition of the ToyRobot
 """
 
 class ToyRobot():
-    pass
+    def __init__(self):
+        self.__xPos = None
+        self.__yPos = None
+        self.__facing = None
+
+    def place(self, x, y, facing):
+        pass
+
+    def move(self):
+        pass
+
+    def right(self):
+        pass
+
+    def left(self):
+        pass
+
+    def report(self):
+        pass
+
+    @staticmethod
+    def __is_on_table(x, y):
+        pass
